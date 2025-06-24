dbb4ae7c0006a1583c3749d977733721d7f25c71
sam-bailie <sam.bailie@hertford.ox.ac.uk>
2025-06-24 20:50:16 +0100

    added ability to turn liter virtual text on and off

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,23 +1,21 @@
-return{
-		"stevearc/conform.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-				local conform = require("conform")
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
-				conform.setup({
-						formatters_by_ft = {
-								python = { "black" },
-						},
-				})
+		conform.setup({
+			formatters_by_ft = {
+				python = { "black" },
+			},
+		})
 
-				vim.keymap.set({ "n", "v" }, "<leader>l", function()
-						conform.format({
-								lsp_fallback = true,
-								async = false,
-								timeout_ms = 500,
-						})
-				end, { desc = "Format file or range (in visual mode)" })
-		end,
+		vim.keymap.set({ "n", "v" }, "<leader>lf", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
-				
-						
--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -6,7 +6,6 @@ return {
 		},
 		config = function()
 				local lint = require("lint")
-				
 				lint.linters_by_ft = {
 						python = { "pylint"},
 				}
@@ -14,7 +13,7 @@ return {
 				local lint_augroup = vim.api.nvim_create_augroup("lint", {clear = true})
 
 				local ns = require("lint").get_namespace("pylint")
-				vim.diagnostic.config({ virtual_text = true}, ns)
+				--vim.diagnostic.config({ virtual_text = false}, ns)
 
 				vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 						group = lint_augroup,
@@ -24,7 +23,17 @@ return {
 				})
 
 				vim.keymap.set("n", "<leader>ll", function()
+						--vim.diagnostic.config({ virtual_text = true}, ns)
 						lint.try_lint()
 				end, { desc = "Trigger linting for current file" })
+
+				vim.keymap.set("n", "<leader>lh", function()
+						vim.diagnostic.config({virtual_text = false})
+				end, { desc = "Turn off virtual text"})
+
+
+				vim.keymap.set("n", "<leader>lc", function()
+						vim.diagnostic.config({virtual_text = true})
+				end, { desc = "Turn on virtual text"})
 		end,
 }
