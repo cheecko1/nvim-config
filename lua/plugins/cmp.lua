e8418d3b2995923c52c98f7afce5e96b76db46e6
Sam Bailie <sam.bailie@amododesign.com>
2025-12-07 09:15:20 +0000

    Code completion

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,54 +1,79 @@
-return{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
-				"hrsh7th/cmp-buffer",
-				"L3MON4D3/LuaSnip",
-				"saadparwaiz1/cmp_luasnip",
-				"rafamadriz/friendly-snippets",
-				"onsails/lspkind.nvim",
-		},
-		config = function()
-				local cmp = require("cmp")
-				
-				local luasnip = require("luasnip")
+-- Code Completion
+return {
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"rafamadriz/friendly-snippets",
+		"onsails/lspkind.nvim",
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		local lspkind = require("lspkind")
 
-				local lspkind = require("lspkind")
+		require("luasnip.loaders.from_vscode").lazy_load()
 
-				require("luasnip.loaders.from_vscode").lazy_load()
+		cmp.setup({
+			completion = {
+				completopt = "menu,menuone,preview,noselect",
+			},
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				["<C-u>"] = cmp.mapping.scroll_docs(-4),
+				["<C-d>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				-- Tab completion to 
+				-- 1. move into the completion menue
+				-- 2. otherwise - jump snippet placeholders
+				-- 3. otherwise - insert a tab
+				["<Tab>"] = function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end,
+				["<S-Tab>"] = function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end,
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+			}),
 
-				cmp.setup({
-						completion = {
-								completopt = "menu,menuone,preview,noselect",
-						},
-						snippet = {
-								expand = function(args)
-										luasnip.lsp_expand(args.body)
-								end,
-						},
-						mapping = cmp.mapping.preset.insert({
-								["<C-p>"] = cmp.mapping.select_prev_item(),
-								["<C-n>"] = cmp.mapping.select_next_item(),
-								["<C-u>"] = cmp.mapping.scroll_docs(-4),
-								["<C-d>"] = cmp.mapping.scroll_docs(4),
-								["<C-Space>"] = cmp.mapping.complete(),
-								["<C-e>"] = cmp.mapping.abort(),
-								["<CR>"] = cmp.mapping.confirm({ select = false }),
-						}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" }, -- higher priority
+				{ name = "luasnip" },
+			}, {
+				{ name = "buffer" }, -- fallback sources
+				{ name = "path" },
+			}),
 
-						sources = cmp.config.sources({
-								{ name = "nvim_lsp" },
-								{ name = "luasnip" },
-								{ name = "buffer" },
-								{ name = "path" },
-						}),
-
-						formatting = {
-								format = lspkind.cmp_format({
-										maxwidth = 50,
-										ellipsis_char = "...",
-								}),
-						},
-				})
-		end,
+			formatting = {
+				format = lspkind.cmp_format({
+					maxwidth = 50,
+					ellipsis_char = "...",
+				}),
+			},
+		})
+	end,
 }
