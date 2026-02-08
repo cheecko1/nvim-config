5433f343ce5df76895fabe0a8fcf3e8dd4504682
cheecko1 <samb19113@gmail.com>
2026-02-08 08:57:15 +0000

    Added typescripts language server

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -2,10 +2,12 @@ return{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 				require("mason-lspconfig").setup({
+						-- manages LSP servers
 						ensure_installed = {
 								'lua_ls',
 								'arduino_language_server',
 								'pyright',
+								'ts_ls',
 						}
 				})
 		end,
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,6 +2,7 @@ return {
 		"williamboman/mason.nvim",
 		config = function()
 				require("mason").setup({
+						-- manages all external tools
 						ensure_installed={
 								'pylint',
 								'clangd',
