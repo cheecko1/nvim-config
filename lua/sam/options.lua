a985fe02af447f6c76e7f53271edb83efeefd980
Sam Bailie <sam.bailie@amododesign.com>
2026-02-11 09:29:38 +0000

    Changed tab to 2 spaces

--- lua/sam/options.lua
+++ lua/sam/options.lua
@@ -3,11 +3,11 @@
 vim.opt.hlsearch = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.number = true		-- show line numbers
+vim.opt.number = true			-- show line numbers
 vim.opt.cursorline = true	-- show line under cursor
-vim.opt.tabstop = 4		-- defaults to 8 spaces for a tab - reduce to 4
-vim.opt.softtabstop = 4		-- defaults to 8 spaces for a tab - reduce to 4
-vim.opt.shiftwidth = 4		-- defaults to 8 spaces for a tab - reduce to 4
+vim.opt.tabstop = 2				-- defaults to 8 spaces for a tab
+vim.opt.softtabstop = 2		-- defaults to 8 spaces for a tab
+vim.opt.shiftwidth = 2		-- defaults to 8 spaces for a tab
 vim.opt.relativenumber = true
 vim.opt.wrap = false
 vim.opt.termguicolors = true
