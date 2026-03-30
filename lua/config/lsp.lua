fbb8a35b7b5bca70bd5a3a74792e02733a439012
Sam Bailie <sam.bailie@amododesign.com>
2026-03-30 11:00:39 +0100

    Generalised lsp keymaps for all LSPs

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,19 +1,24 @@
 -- Global defaults for all LSP servers
+local lsp_keymaps = vim.api.nvim_create_augroup('SamLspKeymaps', { clear = true })
 
-local function lsp_on_attach(client, bufnr)
-  local bufmap = function(mode, lhs, rhs)
-    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr })
-  end
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = lsp_keymaps,
+  callback = function(args)
+    local bufnr = args.buf
+    local bufmap = function(mode, lhs, rhs, desc)
+      vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+    end
 
-  -- Basic LSP keymaps
-  bufmap('n', 'gd', vim.lsp.buf.definition)
-  bufmap('n', 'gD', vim.lsp.buf.declaration)
-  bufmap('n', 'gi', vim.lsp.buf.implementation)
-  bufmap('n', 'gr', vim.lsp.buf.references)
-  bufmap('n', 'K',  vim.lsp.buf.hover)
-  bufmap('n', '<leader>rn', vim.lsp.buf.rename)
-  bufmap('n', '<leader>ca', vim.lsp.buf.code_action)
-end
+    -- Basic LSP keymaps
+    bufmap('n', 'gd', vim.lsp.buf.definition, 'LSP: Go to definition')
+    bufmap('n', 'gD', vim.lsp.buf.declaration, 'LSP: Go to declaration')
+    bufmap('n', 'gi', vim.lsp.buf.implementation, 'LSP: Go to implementation')
+    bufmap('n', 'gr', vim.lsp.buf.references, 'LSP: List references')
+    bufmap('n', 'K', vim.lsp.buf.hover, 'LSP: Hover')
+    bufmap('n', '<leader>rn', vim.lsp.buf.rename, 'LSP: Rename symbol')
+    bufmap('n', '<leader>ca', vim.lsp.buf.code_action, 'LSP: Code action')
+  end,
+})
 
 -- clangd configuration
 vim.lsp.config('clangd', {
@@ -25,12 +30,10 @@ vim.lsp.config('clangd', {
 		},
   filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
   root_markers = { 'compile_commands.json', '.git', '.clang-format', '.clangd' },
-  on_attach = lsp_on_attach,
 })
 
 -- lua_ls configuration (Neovim config development)
 vim.lsp.config('lua_ls', {
-  on_attach = lsp_on_attach,
   settings = {
     Lua = {
       runtime = { version = 'LuaJIT' },
